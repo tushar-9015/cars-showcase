036d61b272e6bc93a36edf71897aab6af45e58ca
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-09-26 02:43:33 +0530

    feat: ComboBox

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { SearchManufacturer } from '.'
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("")
+
+  const handleSearch = () => {
+
+  }
   return (
-    <div>SearchBar</div>
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer 
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
   )
 }
 
--- types/index.ts
+++ types/index.ts
@@ -2,8 +2,17 @@ import { MouseEventHandler } from 'react';
 
 
 export interface CustomButtonProps {
-    title: string,
+    title: string;
     containerStyles?: string,
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
+}
+
+export interface CustomFilterProps {
+    title: string
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
 }
